2359f0f37595d0e24701db3c6010ac6017f4839e
mion007 <mionmax45@gmail.com>
2024-12-28 17:49:11 +0530

    j

--- engine/db.py
+++ engine/db.py
@@ -1,46 +1,49 @@
 import csv
 import sqlite3
 
+# Connect to the SQLite database (or create it if it doesn't exist)
 con = sqlite3.connect("jarvis.db")
 cursor = con.cursor()
 
+# Create the sys_command table if it doesn't exist
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
 
-# query = "INSERT INTO sys_command VALUES (null,'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
-# cursor.execute(query)
-# con.commit()
+# Insert a command into the sys_command table
+query = "INSERT INTO sys_command VALUES (null, 'notepad', 'C:\\Windows\\notepad.exe')"
+cursor.execute(query)
+con.commit()
 
-# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-# cursor.execute(query)
+# Create the web_command table if it doesn't exist
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
 
-# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
-# cursor.execute(query)
-# con.commit()
+# Insert a command into the web_command table
+query = "INSERT INTO web_command VALUES (null, 'youtube', 'https://www.youtube.com/')"
+cursor.execute(query)
+con.commit()
 
-
-# testing module
+# Testing module
 # app_name = "android studio"
 # cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
 # results = cursor.fetchall()
 # print(results[0][0])
 
 # Create a table with the desired columns
-#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
-
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
 
 # Specify the column indices you want to import (0-based index)
 # Example: Importing the 1st and 3rd columns
 # desired_columns_indices = [0, 30]
 
-# # Read data from CSV and insert into SQLite table for the desired columns
+# Read data from CSV and insert into SQLite table for the desired columns
 # with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
 #     csvreader = csv.reader(csvfile)
 #     for row in csvreader:
 #         selected_data = [row[i] for i in desired_columns_indices]
 #         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
 
-# # Commit changes and close connection
+# Commit changes and close connection
 # con.commit()
 # con.close()
 
